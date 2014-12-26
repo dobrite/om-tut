5ec8e31bcd05a2e43bf7a217bf0f4ab7f6d9a89e
David Obrite <david@ministrycentered.com>
2014-12-26 07:32:19 -0800

    editable, done with beginner tut

--- src/om_tut/core.cljs
+++ src/om_tut/core.cljs
@@ -22,6 +22,23 @@
      :6946 "The Structure and Interpretation of Classical Mechanics"
      :1806 "Linear Algebra"}}))
 
+(extend-type string
+  ICloneable
+  (-clone [s] (js/String. s))
+  om/IValue
+  (-value [s] (str s)))
+
+(defn display [show]
+  (if show
+    #js {}
+    #js {:display "none"}))
+
+(defn handle-change [e text owner]
+  (om/transact! text (fn [_] (.. e -target -value))))
+
+(defn commit-change [text owner]
+  (om/set-state! owner :editing false))
+
 (defn middle-name [{:keys [middle middle-initial]}]
   (cond
     middle (str " " middle)
@@ -29,6 +46,27 @@
 
 (defn display-name [{:keys [first last] :as contact}]
   (str last ", " first (middle-name contact)))
+
+(defn editable [text owner]
+  (reify
+    om/IInitState
+    (init-state [_]
+      {:editing false})
+    om/IRenderState
+    (render-state [_ {:keys [editing]}]
+      (dom/li nil
+        (dom/span #js {:style (display (not editing))} (om/value text))
+        (dom/input
+           #js {:style (display editing)
+                :value (om/value text)
+                :onChange #(handle-change % text owner)
+                :onKeyDown #(when (= (.-key %) "Enter")
+                              (commit-change text owner))
+                :onBlur (fn [e] (commit-change text owner))})
+        (dom/button
+         #js {:style (display (not editing))
+              :onClick #(om/set-state! owner :editing true)}
+         "Edit")))))
 
 (defn student-view [student owner]
   (reify
@@ -44,7 +82,7 @@
         (dom/div nil (display-name professor))
         (dom/label nil "Classes")
         (apply dom/ul nil
-          (map #(dom/li nil %) (:classes professor)))))))
+          (om/build-all editable (:classes professor)))))))
 
 (defmulti entry-view (fn [person _] (:type person)))
 
@@ -78,7 +116,7 @@
       (dom/div #js {:id "classes"}
         (dom/h2 nil "Classes")
         (apply dom/ul nil
-          (map #(dom/li nil %) (vals (:classes app))))))))
+          (om/build-all editable (vals (:classes app))))))))
 
 (om/root registry-view app-state
   {:target (. js/document (getElementById "registry"))})
